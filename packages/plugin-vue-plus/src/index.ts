4b66939732968693337e59d39222aacbc8ac81c0
Jiabinbin <425605679@qq.com>
2023-03-16 22:06:54 +0800

    chore: fix eslint error config

--- packages/plugin-vue-plus/src/index.ts
+++ packages/plugin-vue-plus/src/index.ts
@@ -1,103 +1,104 @@
-import path from "path";
-import {readFile} from "fs/promises";
-import {readFileSync} from "fs";
-import {parse} from "./parse";
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { readFileSync } from 'fs';
+import { parse } from './parse';
 
 function getTemplate(template: string) {
-    return `
+  return `
     <template>${template}</template>
-    `
+    `;
 }
 function getScript(template: string) {
-    return `
+  return `
     <script setup lang="ts">${template}</script>
-    `
+    `;
 }
 function getStyle(template: string, lang: string) {
-    return `
+  return `
     <style scoped lang="${lang}">${template}</style>
-    `
+    `;
 }
 
-
 export function angularPlugin() {
-    const fileRegex = /\.(xvue)$/
-    return {
-        name: 'angular-plugin',
-        async transform(code: string, id: string) {
-            if (!fileRegex.test(id)) {
-                return
-            }
-            const mate: any = {}
-            parse(code, mate)
-            if (mate['template'] && mate['templateUrl']) {
-                throw Error('')
-            }
-            // return {code: '<template></template>'}
-            const styles = []
-            if (!mate['template']) {
-                if (mate['templateUrl']) {
-                    const basePath = path.dirname(id)
-                    const url = path.resolve(basePath, mate['templateUrl'])
-                    console.log(url, basePath);
-                    mate['template'] = await readFile(url)
-                } else {
-                    mate['template'] = ''
-                }
-            }
-            if (mate['styles']) {
-                styles.push(...mate['styles'])
-            }
-            // console.log('rrrrr', path.extname(id))
-            let cssLessType = 'css'
-            if (mate['styleUrls']) {
-                // cssLessType
-                styles.push(...mate['styleUrls'].map((v: string) => {
-                    const basePath = path.dirname(id)
-                    const url = path.resolve(basePath, v)
-                    cssLessType = path.extname(url).replace('.', '')
-                    return readFileSync(url).toString()
-                }))
-            }
-            const template = mate['template']
-            const script: string[] = []
+  const fileRegex = /\.(xvue)$/;
+  return {
+    name: 'angular-plugin',
+    async transform(code: string, id: string) {
+      if (!fileRegex.test(id)) {
+        return;
+      }
+      const mate: any = {};
+      parse(code, mate);
+      if (mate['template'] && mate['templateUrl']) {
+        throw Error('');
+      }
+      // return {code: '<template></template>'}
+      const styles = [];
+      if (!mate['template']) {
+        if (mate['templateUrl']) {
+          const basePath = path.dirname(id);
+          const url = path.resolve(basePath, mate['templateUrl']);
+          console.log(url, basePath);
+          mate['template'] = await readFile(url);
+        } else {
+          mate['template'] = '';
+        }
+      }
+      if (mate['styles']) {
+        styles.push(...mate['styles']);
+      }
+      // console.log('rrrrr', path.extname(id))
+      let cssLessType = 'css';
+      if (mate['styleUrls']) {
+        // cssLessType
+        styles.push(
+          ...mate['styleUrls'].map((v: string) => {
+            const basePath = path.dirname(id);
+            const url = path.resolve(basePath, v);
+            cssLessType = path.extname(url).replace('.', '');
+            return readFileSync(url).toString();
+          }),
+        );
+      }
+      const template = mate['template'];
+      const script: string[] = [];
 
-            if (mate['lifecycleHook']) {
-                const unImports: string[] = []
+      if (mate['lifecycleHook']) {
+        const unImports: string[] = [];
 
-                mate['lifecycleHook'].forEach((value: { name: string; }) => {
-                    const {name} = value
-                    if (mate.vueDepends.includes(name)) {
-                        return
-                    }
+        mate['lifecycleHook'].forEach((value: { name: string }) => {
+          const { name } = value;
+          if (mate.vueDepends.includes(name)) {
+            return;
+          }
 
-                    unImports.push(name)
-                })
+          unImports.push(name);
+        });
 
-                if (unImports.length) {
-                    script.unshift(`\nimport { ${unImports.join(', ')} } from 'vue'`)
-                }
-            }
+        if (unImports.length) {
+          script.unshift(`\nimport { ${unImports.join(', ')} } from 'vue'`);
+        }
+      }
 
-            const unImports = []
+      const unImports = [];
 
-            if (!mate.diDepends.includes('attachInjector')) {
-                unImports.push('attachInjector')
-            }
+      if (!mate.diDepends.includes('attachInjector')) {
+        unImports.push('attachInjector');
+      }
 
-            if (mate.needInjectableInjector && !mate.diDepends.includes('attachInjectableInjector')) {
-                unImports.push('attachInjectableInjector')
-            }
+      if (mate.needInjectableInjector && !mate.diDepends.includes('attachInjectableInjector')) {
+        unImports.push('attachInjectableInjector');
+      }
 
-            if (unImports.length) {
-                script.unshift(`\nimport { ${unImports.join(', ')} } from 'vue-plus'`)
-            }
+      if (unImports.length) {
+        script.unshift(`\nimport { ${unImports.join(', ')} } from 'vue-plus'`);
+      }
 
-            if (mate['newSourceCode']) {
-                script.push(`\n${mate['newSourceCode']}`)
-            }
+      if (mate['newSourceCode']) {
+        script.push(`\n${mate['newSourceCode']}`);
+      }
 
-            script.push(`\nObject.defineProperty(${mate['class']}, '__decorator__', {
+      script.push(`\nObject.defineProperty(${mate['class']}, '__decorator__', {
     value: {
         providers: [
             ${mate['providers'].join(',')}
@@ -107,45 +108,45 @@ export function angularPlugin() {
     configurable: false,
     writable: false,
     enumerable: false
-})`)
+})`);
 
-            if (mate['class']) {
-                script.push(`\nconst component = attachInjector(${mate['class']})`)
-            }
+      if (mate['class']) {
+        script.push(`\nconst component = attachInjector(${mate['class']})`);
+      }
 
-            const exposed: string[] = []
+      const exposed: string[] = [];
 
-            if (mate['properties']) {
-                mate['properties'].forEach((value: { name: string; }) => {
-                    exposed.push(value.name)
-                })
-            }
+      if (mate['properties']) {
+        mate['properties'].forEach((value: { name: string }) => {
+          exposed.push(value.name);
+        });
+      }
 
-            if (mate['methods']) {
-                mate['methods'].forEach((value: { name: string; }) => {
-                    exposed.push(value.name)
-                })
-            }
+      if (mate['methods']) {
+        mate['methods'].forEach((value: { name: string }) => {
+          exposed.push(value.name);
+        });
+      }
 
-            if (mate['lifecycleHook']) {
-                exposed.push(...mate['lifecycleHook'].map((value: { name: any; }) => `${value.name}: $$${value.name}`))
-            }
+      if (mate['lifecycleHook']) {
+        exposed.push(...mate['lifecycleHook'].map((value: { name: any }) => `${value.name}: $$${value.name}`));
+      }
 
-            if (exposed.length) {
-                script.push(`\nconst { ${exposed.join(', ')} } = component`)
-            }
+      if (exposed.length) {
+        script.push(`\nconst { ${exposed.join(', ')} } = component`);
+      }
 
-            mate['lifecycleHook'].forEach((value: { name: string; }) => {
-                script.push(`\n${value.name}($$${value.name}.bind(component))`)
-            });
-            console.log({
-                code: `${getTemplate(template)} \n ${getScript(script.join(''))}\n ${getStyle(styles.join(''), cssLessType)}\n`
-            })
+      mate['lifecycleHook'].forEach((value: { name: string }) => {
+        script.push(`\n${value.name}($$${value.name}.bind(component))`);
+      });
+      console.log({
+        code: `${getTemplate(template)} \n ${getScript(script.join(''))}\n ${getStyle(styles.join(''), cssLessType)}\n`,
+      });
 
-            return {
-                code: `${getTemplate(template)} \n ${getScript(script.join(''))}\n ${getStyle(styles.join(''), cssLessType)}\n`,
-                // map: sourceMap.toString()
-            }
-        }
-    }
+      return {
+        code: `${getTemplate(template)} \n ${getScript(script.join(''))}\n ${getStyle(styles.join(''), cssLessType)}\n`,
+        // map: sourceMap.toString()
+      };
+    },
+  };
 }
--- packages/vue-plus/src/component.ts
+++ packages/vue-plus/src/component.ts
@@ -1,130 +1,126 @@
-import type {ComponentPublicInstance, InjectionKey} from "vue";
-import type {ReactiveEffect, TrackOpTypes, TriggerOpTypes} from "vue";
+import type { ComponentPublicInstance, InjectionKey } from 'vue';
+import type { ReactiveEffect, TrackOpTypes, TriggerOpTypes } from 'vue';
 
 export interface Type<T> extends Function {
-    new(...args: any[]): T;
+  new (...args: any[]): T;
 }
 
 export interface TypeDecorator {
-    <T extends Type<any>>(type: T): T;
+  <T extends Type<any>>(type: T): T;
 
-    (target: Object, propertyKey?: string|symbol, parameterIndex?: number): void;
+  (target: Object, propertyKey?: string | symbol, parameterIndex?: number): void;
 }
 
 export interface ComponentDecorator {
-    (obj: Component): TypeDecorator;
-    new(obj: Component): Component;
+  (obj: Component): TypeDecorator;
+  new (obj: Component): Component;
 }
 
 export interface InjectDecorator {
-    (token: any): any;
-    new(token: any): any;
+  (token: any): any;
+  new (token: any): any;
 }
 
 type DebuggerEvent = {
-    effect: ReactiveEffect
-    target: object
-    type: TrackOpTypes /* 'get' | 'has' | 'iterate' */
-    key: any
-}
+  effect: ReactiveEffect;
+  target: object;
+  type: TrackOpTypes /* 'get' | 'has' | 'iterate' */;
+  key: any;
+};
 
 type DebuggerRenderEvent = {
-    effect: ReactiveEffect
-    target: object
-    type: TriggerOpTypes /* 'set' | 'add' | 'delete' | 'clear' */
-    key: any
-    newValue?: any
-    oldValue?: any
-    oldTarget?: Map<any, any> | Set<any>
-}
+  effect: ReactiveEffect;
+  target: object;
+  type: TriggerOpTypes /* 'set' | 'add' | 'delete' | 'clear' */;
+  key: any;
+  newValue?: any;
+  oldValue?: any;
+  oldTarget?: Map<any, any> | Set<any>;
+};
 export interface LifecycleHook {
-    onMounted?(): void
+  onMounted?(): void;
 
-    onUpdated?(): void
+  onUpdated?(): void;
 
-    onUnmounted?(): void
+  onUnmounted?(): void;
 
-    onBeforeMount?(): void
+  onBeforeMount?(): void;
 
-    onBeforeUpdate?(): void
+  onBeforeUpdate?(): void;
 
-    onBeforeUnmount?(): void
+  onBeforeUnmount?(): void;
 
-    onErrorCaptured?(err: unknown, instance: ComponentPublicInstance | null, info: string): boolean | void
+  onErrorCaptured?(err: unknown, instance: ComponentPublicInstance | null, info: string): boolean | void;
 
-    onRenderTracked?(e: DebuggerEvent): void
+  onRenderTracked?(e: DebuggerEvent): void;
 
-    onRenderTriggered?(e: DebuggerRenderEvent): void
+  onRenderTriggered?(e: DebuggerRenderEvent): void;
 
-    onActivated?(): void
+  onActivated?(): void;
 
-    onDeactivated?(): void
+  onDeactivated?(): void;
 
-    onServerPrefetch?(): Promise<any>
+  onServerPrefetch?(): Promise<any>;
 }
 
 export function InjectionToken<T = any>(desc?: string): InjectionKey<T> {
-    return Symbol(desc)
+  return Symbol(desc);
 }
 export interface ValueSansProvider {
-    useValue: any;
+  useValue: any;
 }
 
 export declare interface ValueProvider extends ValueSansProvider {
-    provide: any;
-    multi?: boolean;
+  provide: any;
+  multi?: boolean;
 }
 
-
-export interface TypeProvider extends Type<any> {
-}
+export interface TypeProvider extends Type<any> {}
 
 export declare interface ClassSansProvider {
-    useClass: Type<any>;
+  useClass: Type<any>;
 }
 
 export declare interface ClassProvider extends ClassSansProvider {
-    provide: any;
-    multi?: boolean;
+  provide: any;
+  multi?: boolean;
 }
 
 export type FactoryProvider<T = any> = {
-    provide: any;
-    useFactory: (...args: any[]) => T;
-    deps?: any[];
-    multi?: boolean;
+  provide: any;
+  useFactory: (...args: any[]) => T;
+  deps?: any[];
+  multi?: boolean;
 };
 
 export type Provider = TypeProvider | ValueProvider | ClassProvider | FactoryProvider;
 export interface Component {
-    templateUrl?: string;
-    template?: string;
-    styleUrls?: string[];
-    selector?: string;
-    styles?: string[];
-    providers?: Provider[],
-    components?: any[]
+  templateUrl?: string;
+  template?: string;
+  styleUrls?: string[];
+  selector?: string;
+  styles?: string[];
+  providers?: Provider[];
+  components?: any[];
 }
 
 export function Ref(): PropertyDecorator {
-    return function () {
-
-    }
+  return function () {};
 }
 
-export const Component: ComponentDecorator = null as unknown as ComponentDecorator
+export const Component: ComponentDecorator = null as unknown as ComponentDecorator;
 export const Inject: InjectDecorator = function (token: string): ParameterDecorator {
-    return (target, propertyKey, parameterIndex) => void 0
-} as unknown as InjectDecorator
+  return (target, propertyKey, parameterIndex) => void 0;
+} as unknown as InjectDecorator;
 export const Host = function (): ParameterDecorator {
-    return (target, propertyKey, parameterIndex) => void 0
-}
+  return (target, propertyKey, parameterIndex) => void 0;
+};
 export const SkipSelf = function (): ParameterDecorator {
-    return (target, propertyKey, parameterIndex) => void 0
-}
+  return (target, propertyKey, parameterIndex) => void 0;
+};
 export const Self = function (): ParameterDecorator {
-    return (target, propertyKey, parameterIndex) => void 0
-}
+  return (target, propertyKey, parameterIndex) => void 0;
+};
 export const Optional = function (): ParameterDecorator {
-    return (target, propertyKey, parameterIndex) => void 0
-}
+  return (target, propertyKey, parameterIndex) => void 0;
+};
